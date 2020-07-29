0eb11e9a972c57a8bb8ad6de2cdc238a4ae7d567
#emmyCode <emmyCode4495@gmail.com>
2020-07-29 13:05:48 +0100

    Created a Product detail view

--- online/shop_cart/views.py
+++ online/shop_cart/views.py
@@ -16,3 +16,12 @@ def product_list(request, category_slug=None):
                  'categories',categories,
                  'products':'products'})
 
+# Function view to display a single product
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product,
+                                id=id,
+                                slug=slug,
+                                available=True)
+    return render(request,
+                'product/detail.html',
+                {'product':product})
