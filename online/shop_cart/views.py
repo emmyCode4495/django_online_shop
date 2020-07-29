b84e6f4dbc983ff0891d9b3f197a0dbfe68c425d
#emmyCode <emmyCode4495@gmail.com>
2020-07-29 12:55:09 +0100

    Created a Product list view

--- online/shop_cart/views.py
+++ online/shop_cart/views.py
@@ -5,4 +5,14 @@ from .models import Category, Product
 # Creating the catalog view
 def product_list(request, category_slug=None):
     category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request,
+                 'product/list.html',
+                 {'category':category,
+                 'categories',categories,
+                 'products':'products'})
 
