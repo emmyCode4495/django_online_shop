c9c5185f9bb594cd71c3ef7c8a70d1e2bff2ddff
#emmyCode <emmyCode4495@gmail.com>
2020-08-01 01:33:56 +0100

    Added  the add_to-cart button to the shop_cart views.py and detail.html

--- online/shop_cart/views.py
+++ online/shop_cart/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-
+from cart.forms import CartAddProductForm
 from .models import Category, Product
 
 # Creating the catalog view
@@ -22,6 +22,8 @@ def product_detail(request, id, slug):
                                 id=id,
                                 slug=slug,
                                 available=True)
+    cart_product_form = CartAddProductForm()
     return render(request,
                 'product/detail.html',
-                {'product':product})
+                {'product':product,
+                'cart_product_form': cart_product_form})
